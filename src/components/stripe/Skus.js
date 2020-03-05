52f54c80056b34610f89bb0a28f4fa276705dc7a
arhoy <you@example.com>
2020-03-05 01:27:58 -0700

    updated pricing page

--- src/components/stripe/Skus.js
+++ src/components/stripe/Skus.js
@@ -1,59 +1,59 @@
-import React, { Component } from 'react';
-import { graphql, StaticQuery } from 'gatsby';
-import SkuCard from './SkuCard';
-import styled from '@emotion/styled';
+// import React, { Component } from 'react';
+// import { graphql, StaticQuery } from 'gatsby';
+// import SkuCard from './SkuCard';
+// import styled from '@emotion/styled';
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  justify-content: center;
-  align-items: center;
-`;
+// const Container = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   flex-wrap: wrap;
+//   justify-content: center;
+//   align-items: center;
+// `;
 
-class Skus extends Component {
-  state = {
-    stripe: null,
-  };
+// class Skus extends Component {
+//   state = {
+//     stripe: null,
+//   };
 
-  // Initialise Stripe.js with your publishable key.
-  // You can find your key in the Dashboard:
-  // https://dashboard.stripe.com/account/apikeys
-  componentDidMount() {
-    const stripe = window.Stripe(process.env.GATSBY_STRIPE_PUBLIC_KEY);
-    this.setState({ stripe });
-  }
+//   // Initialise Stripe.js with your publishable key.
+//   // You can find your key in the Dashboard:
+//   // https://dashboard.stripe.com/account/apikeys
+//   componentDidMount() {
+//     const stripe = window.Stripe(process.env.GATSBY_STRIPE_PUBLIC_KEY);
+//     this.setState({ stripe });
+//   }
 
-  render() {
-    return (
-      <StaticQuery
-        query={graphql`
-          query SkusForProduct {
-            skus: allStripeSku(sort: { fields: [price] }) {
-              edges {
-                node {
-                  id
-                  currency
-                  price
-                  image
-                  attributes {
-                    name
-                  }
-                }
-              }
-            }
-          }
-        `}
-        render={({ skus }) => (
-          <Container>
-            {skus.edges.map(({ node: sku }) => (
-              <SkuCard key={sku.id} sku={sku} stripe={this.state.stripe} />
-            ))}
-          </Container>
-        )}
-      />
-    );
-  }
-}
+//   render() {
+//     return (
+//       <StaticQuery
+//         query={graphql`
+//           query SkusForProduct {
+//             skus: allStripeSku(sort: { fields: [price] }) {
+//               edges {
+//                 node {
+//                   id
+//                   currency
+//                   price
+//                   image
+//                   attributes {
+//                     name
+//                   }
+//                 }
+//               }
+//             }
+//           }
+//         `}
+//         render={({ skus }) => (
+//           <Container>
+//             {skus.edges.map(({ node: sku }) => (
+//               <SkuCard key={sku.id} sku={sku} stripe={this.state.stripe} />
+//             ))}
+//           </Container>
+//         )}
+//       />
+//     );
+//   }
+// }
 
-export default Skus;
+// export default Skus;
